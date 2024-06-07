5c46a641395a1a0e85ce6e086e4c2e07adf9195a
wowlikon <wowlikon@gmail.com>
2024-06-07 17:11:16 +0300

    get address fix

--- README.md
+++ README.md
@@ -2,17 +2,25 @@
 Сервер может сохранять данные из HTTP POST запросов и находить данные по id
 
 ### POST `/address`
+Позволяет отправить данные на сервер
 ```bash
 curl -X POST --json '{"name": "alex", "address": "home", "longitude": 123, "latitude": 456}' http://localhost:8080/address
 
 {"id": 1234567890}
 ```
 
-### GET `/address`
+### GET `/address/:id`
+Позволяет получить данные по id
 ```bash
-curl -X GEt --json '{"id": 1234567890}' http://localhost:8080/address
+curl -X GET http://localhost:8080/address/1234567890
 
 {"name": "alex", "address": "home", "longitude": 123, "latitude": 456}
 ```
 
-# ⚠️Сервер ещё на этапе разработки и может работать не стабильно/не работать⚠️
+### GET `/all`
+Позволяет получить все данные
+```bash
+curl -X GET http://localhost:8080/all
+
+{"1234567890":{"name":"alex","address":"home","longitude":123,"latitude":456}}
+```
--- main.go
+++ main.go
@@ -1,4 +1,3 @@
-
 package main
 
 import (
@@ -6,20 +5,15 @@ import (
 	"math/rand"
 	"net/http"
 	"strconv"
-	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 type Address struct {
-	Name string `json:"name"`
-	Address string `json: "address"`
-	Longitude float64 `json: "longitude"`
-	Latitude float64 `json: "latitude"`
-}
-
-type Query struct {
-	id string `json: "id"`
+	Name      string  `json:"name"`
+	Address   string  `json:"address"`
+	Longitude float64 `json:"longitude"`
+	Latitude  float64 `json:"latitude"`
 }
 
 func NewID() string {
@@ -31,13 +25,13 @@ var addresses map[string]Address
 
 func main() {
 	addresses = make(map[string]Address)
-	rand.Seed(time.Now().UnixNano())
 	fmt.Println(NewID())
 
 	r := gin.Default()
 	r.GET("/all", func(c *gin.Context) {
 		c.JSON(http.StatusOK, addresses)
 	})
+
 	r.POST("/address", func(c *gin.Context) {
 		var addr Address
 		id := NewID()
@@ -48,20 +42,16 @@ func main() {
 		addresses[id] = addr
 		c.JSON(http.StatusOK, gin.H{"id": id})
 	})
-	r.GET("/address", func(c *gin.Context) {
-		var q Query
-		if err := c.Bind(&q); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
 
-		addr, ok := addresses[q.id]
+	r.GET("/address/:id", func(c *gin.Context) {
+		addr, ok := addresses[c.Param("id")]
 		if !ok {
 			c.JSON(http.StatusNotFound, gin.H{"error": "not found"})
 			return
 		}
-		
+
 		c.JSON(http.StatusOK, addr)
 	})
-  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	r.Run()
 }
